ec9258dcf3a2ca648c87c595752ea57eb9f9b943
Kamil Smuga <smugakamil@gmail.com>
2015-09-26 14:03:01 +0100

    insert doSth() at current position

--- nodejs_autocomplete.py
+++ nodejs_autocomplete.py
@@ -3,4 +3,11 @@ import sublime_plugin
 
 class NodejsAutocompleteCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+		view = self.view
+		view_sel = view.sel()
+		if not view_sel:
+			return
+
+		pos = view_sel[0].begin()
+		self.view.insert(edit, pos, ".doSth()")
+
